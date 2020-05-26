8e63eb43ba58edbdd92f58b01da2813ce8c53984
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-05-26 15:05:31 -0400

    Updates

---
+++ precache-manifest.8b83b85e207a0c1918419bd0d8ab0e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5378fbcb066e90a6d5ed1ce2f3fde1d",
+    "url": "/markdown-react/index.html"
+  },
+  {
+    "revision": "dc850dd1f665a7e64870",
+    "url": "/markdown-react/static/css/main.61c0c8a5.chunk.css"
+  },
+  {
+    "revision": "4ba4f17ce0418228a40f",
+    "url": "/markdown-react/static/js/2.805704a2.chunk.js"
+  },
+  {
+    "revision": "ef4d37cce893e0672c389c5cdb0258c9",
+    "url": "/markdown-react/static/js/2.805704a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc850dd1f665a7e64870",
+    "url": "/markdown-react/static/js/main.8760efe0.chunk.js"
+  },
+  {
+    "revision": "7e2571fa633349d310be",
+    "url": "/markdown-react/static/js/runtime-main.42dbb78e.js"
+  }
+]);
